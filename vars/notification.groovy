090d5bacf6fb9271892c4d2a793db0b5ecb953c5
Bjoern Eirik Benjaminsen <Bjoern Eirik Benjaminsen>
2017-03-02 22:04:46 +0100

    Initial

---
+++ vars/notification.groovy
@@ -0,0 +1,17 @@
+#!groovy 
+
+//Somewhat based on https://github.com/bitwiseman/jenkins-pipeline-shared/blob/master/vars/sendNotifications.groovy
+
+def call(String buildStatus = 'STARTED') {
+	// build status of null means successful
+	 buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+	def color =     color = '#FF0000'
+
+ 	if (buildStatus == 'SUCCESSFUL') {
+    	color = '#00FF00'
+  	} else {
+    	color = '#FF0000'
+ 	 }
+  	slackSend(color: color, message: "Job ${env.JOB_NAME} ${buildStatus}"
+}
